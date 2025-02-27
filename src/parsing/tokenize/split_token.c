b0e162acfd7bb8d216bac65240ae3079e21fa6b9
Thomasrbm <frozyxyt.76@gmail.com>
2025-02-27 19:23:03 +0100

    Add files via upload

---
+++ src/parsing/tokenize/split_token.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: throbert <throbert@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/23 00:48:23 by throbert          #+#    #+#             */
+/*   Updated: 2025/02/27 18:32:56 by throbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+static int	count_tokens(char *in)
+{
+	int	i;
+	int	cnt;
+
+	i = 0;
+	cnt = 0;
+	while (in[i])
+	{
+		while (in[i] && ft_isspace(in[i]))
+			i++;
+		if (!in[i])
+			break ;
+		while (in[i] && !ft_isspace(in[i]))
+			i++;
+		cnt++;
+	}
+	return (cnt);
+}
+
+char	**ft_split_token(char *in, t_shell *shell)
+{
+	char	**tokens;
+	int		cnt;
+	int		i;
+	int		n;
+	char	*word;
+
+	in = parse_cmd(in);
+	cnt = count_tokens(in);
+	tokens = malloc(sizeof(char *) * (cnt + 1));
+	if (!tokens)
+		return (NULL);
+	n = 0;
+	i = 0;
+	while (in[i])
+	{
+		while (in[i] && ft_isspace(in[i]))
+			i++;
+		if (!in[i])
+			break ;
+		word = ft_parse_word(in, &i, shell);
+		tokens[n++] = word;
+	}
+	tokens[n] = NULL;
+	free(in);
+	return (tokens);
+}
